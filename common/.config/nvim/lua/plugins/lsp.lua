b415f1cbd0728ad2f7403972cdbfe56e5c8357ed
qiyoudaoyi <qiyoudaoyi@gmail.com>
2025-01-29 16:43:40 -0800

    remove java lsp

--- common/.config/nvim/lua/plugins/lsp.lua
+++ common/.config/nvim/lua/plugins/lsp.lua
@@ -1,170 +1,169 @@
 return {
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v4.x",
-		lazy = true,
-		config = false,
-	},
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = true,
-	},
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v4.x",
+    lazy = true,
+    config = false,
+  },
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = true,
+  },
 
-	-- Autocompletion
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = { "L3MON4D3/LuaSnip", "onsails/lspkind-nvim" },
-		config = function()
-			local cmp = require("cmp")
-			local lspkind = require("lspkind")
-			local cmp_action = require("lsp-zero").cmp_action()
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = { "L3MON4D3/LuaSnip", "onsails/lspkind-nvim" },
+    config = function()
+      local cmp = require("cmp")
+      local lspkind = require("lspkind")
+      local cmp_action = require("lsp-zero").cmp_action()
 
-			local has_words_before = function()
-				if vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt" then
-					return false
-				end
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-			end
+      local has_words_before = function()
+        if vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt" then
+          return false
+        end
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0
+            and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+      end
 
-			cmp.setup({
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "copilot" },
-				},
-				formatting = {
-					format = lspkind.cmp_format({
-						mode = "symbol",
-						max_width = 50,
-						symbol_map = { Copilot = "" },
-					}),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<CR>"] = cmp.mapping.confirm({
-						-- documentation says this is important.
-						-- I don't know why.
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = false,
-					}),
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
-					["<C-f>"] = cmp_action.luasnip_jump_forward(),
-					["<C-b>"] = cmp_action.luasnip_jump_backward(),
-					["<C-l>"] = cmp.mapping.complete(),
-					["<C-j>"] = function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item({
-								behavior = cmp.SelectBehavior.Select,
-							})
-						else
-							fallback()
-						end
-					end,
-					["<C-k>"] = function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item({
-								behavior = cmp.SelectBehavior.Select,
-							})
-						else
-							fallback()
-						end
-					end,
-					["<Tab>"] = vim.schedule_wrap(function(fallback)
-						if cmp.visible() and has_words_before() then
-							cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-						else
-							fallback()
-						end
-					end),
-				}),
-				snippet = {
-					expand = function(args)
-						vim.snippet.expand(args.body)
-					end,
-				},
-			})
-		end,
-	},
+      cmp.setup({
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "copilot" },
+        },
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol",
+            max_width = 50,
+            symbol_map = { Copilot = "" },
+          }),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({
+            -- documentation says this is important.
+            -- I don't know why.
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = false,
+          }),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+          ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+          ["<C-l>"] = cmp.mapping.complete(),
+          ["<C-j>"] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item({
+                behavior = cmp.SelectBehavior.Select,
+              })
+            else
+              fallback()
+            end
+          end,
+          ["<C-k>"] = function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item({
+                behavior = cmp.SelectBehavior.Select,
+              })
+            else
+              fallback()
+            end
+          end,
+          ["<Tab>"] = vim.schedule_wrap(function(fallback)
+            if cmp.visible() and has_words_before() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              fallback()
+            end
+          end),
+        }),
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
+      })
+    end,
+  },
 
-	-- LSP
-	{
-		"neovim/nvim-lspconfig",
-		cmd = { "LspInfo", "LspInstall", "LspStart" },
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-			"williamboman/mason-lspconfig.nvim",
-			"kevinhwang91/nvim-ufo",
-			"aznhe21/actions-preview.nvim",
-		},
-		opts = { inlay_hints = { enable = true } },
-		config = function()
-			-- This is where all the LSP shenanigans will live
-			local lsp_zero = require("lsp-zero")
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    cmd = { "LspInfo", "LspInstall", "LspStart" },
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "williamboman/mason-lspconfig.nvim",
+      "kevinhwang91/nvim-ufo",
+      "aznhe21/actions-preview.nvim",
+    },
+    opts = { inlay_hints = { enable = true } },
+    config = function()
+      -- This is where all the LSP shenanigans will live
+      local lsp_zero = require("lsp-zero")
 
-			local lsp_attach = function(client, bufnr)
-				lsp_zero.default_keymaps({ buffer = bufnr })
-			end
+      local lsp_attach = function(client, bufnr)
+        lsp_zero.default_keymaps({ buffer = bufnr })
+      end
 
-			local lsp_capabilities = vim.tbl_deep_extend("force", require("cmp_nvim_lsp").default_capabilities(), {
-				textDocument = {
-					foldingRange = {
-						dynamicRegistration = false,
-						lineFoldingOnly = true,
-					},
-				},
-			})
+      local lsp_capabilities = vim.tbl_deep_extend("force", require("cmp_nvim_lsp").default_capabilities(), {
+        textDocument = {
+          foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true,
+          },
+        },
+      })
 
-			lsp_zero.extend_lspconfig({
-				sign_text = {
-					error = "✘",
-					warn = "▲",
-					hint = "⚑",
-					info = "»",
-				},
-				lsp_attach = lsp_attach,
-				capabilities = lsp_capabilities,
-			})
+      lsp_zero.extend_lspconfig({
+        sign_text = {
+          error = "✘",
+          warn = "▲",
+          hint = "⚑",
+          info = "»",
+        },
+        lsp_attach = lsp_attach,
+        capabilities = lsp_capabilities,
+      })
 
-			require("mason").setup({})
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"ts_ls",
-					"clangd",
-					"rust_analyzer",
-					"eslint",
-					"lua_ls",
-					"svelte",
-					"tailwindcss",
-					"jdtls",
-					"texlab",
-				},
-				handlers = {
-					function(server_name)
-						require("lspconfig")[server_name].setup({})
-					end,
-					tailwindcss = function()
-						require("lspconfig").tailwindcss.setup({
-							root_dir = function(...)
-								return require("lspconfig.util").root_pattern(".git")(...)
-							end,
-						})
-					end,
-					ts_ls = function()
-						require("lspconfig").ts_ls.setup({
-							root_dir = function(...)
-								return require("lspconfig.util").root_pattern(".git")(...)
-							end,
-							single_file_support = false,
-						})
-					end,
-				},
-			})
-		end,
-	},
+      require("mason").setup({})
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "ts_ls",
+          "clangd",
+          "rust_analyzer",
+          "eslint",
+          "lua_ls",
+          "svelte",
+          "tailwindcss",
+          "texlab",
+        },
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup({})
+          end,
+          tailwindcss = function()
+            require("lspconfig").tailwindcss.setup({
+              root_dir = function(...)
+                return require("lspconfig.util").root_pattern(".git")(...)
+              end,
+            })
+          end,
+          ts_ls = function()
+            require("lspconfig").ts_ls.setup({
+              root_dir = function(...)
+                return require("lspconfig.util").root_pattern(".git")(...)
+              end,
+              single_file_support = false,
+            })
+          end,
+        },
+      })
+    end,
+  },
 }
